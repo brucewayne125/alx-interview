13fbc7825d5c274ac0c55bc8394bdef7e0357bf7
brucewayne125 <snowdbat@gmail.com>
2024-11-12 04:13:47 -0800

    Task0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def validUTF8(data):
+    number_in_bytes = 0
+
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for byte in data:
+        byte = byte & 0xFF
+
+        if number_in_bytes == 0:
+            if(byte & mask1) == 0:
+                continue
+            elif(byte & (mask1 >> 1)) == mask1:
+                number_in_bytes = 1
+            elif(byte & (mask1 >> 2)) == (mask1 >> 1):
+                number_in_bytes = 2
+            elif(byte & (mask1 >> 3)) == (mask1 >> 2):
+                number_in_bytes = 3
+            else:
+                return False
+        else:
+            if not(byte & mask1 and not (byte & mask2)):
+                return False
+            number_in_bytes = number_in_bytes - 1
+    
+    return number_in_bytes == 0
